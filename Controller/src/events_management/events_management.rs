45698c758b1c8420d7f45713db34eeee6f0e4f87
Sw0ocHCorp <96880567+Sw0ocHCorp@users.noreply.github.com>
2025-05-12 21:18:27 +0200

    Lambda or impl function as Observer respond function
    - The function set_callback can have lambda function or another impl as argument

--- Controller/src/events_management/events_management.rs
+++ Controller/src/events_management/events_management.rs
@@ -3,19 +3,40 @@ use bevy::ecs::observer;
 #[allow(dead_code)]
 pub struct Observer<T> {
     _marker: std::marker::PhantomData<T>,
+    callback_fun: Option<Box<dyn Fn(&T)>>,
 }
-
+#[allow(dead_code)]
 impl<T> Observer<T> {
     pub fn new() -> Self {
-        let obs= Observer { _marker: std::marker::PhantomData };
+        let obs= Observer { _marker: std::marker::PhantomData, callback_fun: None };
         return obs;
     }
+    
+    pub fn set_callback<F: Fn(&T) + 'static>(&mut self, f: F) {
+        // Set the callback function
+        self.callback_fun = Some(Box::new(f));
+    }
+
     fn respond(&self, data: &T) {
-        std::println!("Observer received data:");
+        if self.callback_fun.is_some() {
+            // Call the callback function with the data
+            (self.callback_fun.as_ref().unwrap())(data);
+        } else {
+            // Handle the case where no callback function is set
+            std::println!("No callback function set for this observer.");
+        }
+    }
+}
+#[allow(dead_code)]
+pub trait SpecificCallbacks {
+    fn test(&self, data: &String) {
+        println!("Received data: {}", data);
     }
 }
 
+
 #[allow(dead_code)]
+//------------- GENERIC EVENTS & OBSERVERS -------------//
 pub trait Event<T> {
     fn new() -> Self;
     fn trigger(&self, data: &T) {
@@ -44,3 +65,5 @@ impl<T> Event<T> for CustomEvent<T> {
         }
     }
 }
+
+//------------- SPECIFIC EVENTS & OBSERVERS -------------//
--- Controller/src/main.rs
+++ Controller/src/main.rs
@@ -9,6 +9,11 @@ use events_management::{CustomEvent, Event, Observer};
 
 fn main() {
     let mut test_event = events_management::CustomEvent::new();
-    test_event.add_observer(events_management::Observer::new());
+    let mut observer = events_management::Observer::new();
+    //We can pass Lambda Function or function of another impl as argument
+    observer.set_callback(|data: &String| {
+        println!("Received: {}", data);
+    });
+    test_event.add_observer(observer);
     test_event.trigger(&("Hello".to_string()));
 }
